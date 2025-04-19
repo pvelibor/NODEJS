06a10f7f89ddfea4e06079b92958016c1bb92bd1
pvelibor <vesasmile@gmail.com>
2025-04-19 20:00:05 +0200

    second commit

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
+var varNum1 = 11;
+var varNum2 = true;
+var varNum3 ="Velibor Popović";
+var varNum4 = ["Velibor", "Marko", "Aleksandar"];
+var varNum5 = Symbol("buton");
+var varNum6 = null;
+var varNum7 = BigInt (847628734628374623874);
 
+
+const varNum8 = 15;
+const varNum9 = false;
+const varNum10 = 'something diferent';
+const varNUm11 = [2, 5, 7];
+const varNum12 = Symbol ("!");
+const varNum13 = null;
+const carNum14 = BigInt (800008);
 
--- test.ts
+++ test.ts
@@ -2,4 +2,19 @@ var varNum1: number = 10;
 var varNum2: boolean = true;
 var varNum3: string ="Velibor Popović";
 var varNum4: string[] = ["Velibor", "Marko", "Aleksandar"];
-var varNum5: 
+var varNum5 = Symbol("buton");
+var varNum6 = null;
+var varNum7 = BigInt (847628734628374623874);
+
+
+let varNum8: number = 15;
+let varNum9: boolean = false;
+let varNum10: string = 'something diferent';
+let varNUm11: number[] = [2, 5, 7];
+let varNum12 = Symbol ("!");
+let varNum13 = null;
+let carNum14 = BigInt (800008);
+
+//Node version for this Ubuntu system is v18.19.1.
+//Node.js is an cross-platform JavaScript runtime environment which is primarily used for backend development
+
